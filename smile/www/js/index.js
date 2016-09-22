de9e68bb59d3d288db9c7de2fee78f41072c5483
彭清峰 <985200674@qq.com>
2016-09-22 20:39:41 +0800

    smile

---
+++ smile/www/js/index.js
@@ -0,0 +1,18 @@
+console.log( $.cookie())
+var uname = $.cookie('uname')
+if(uname){
+    $('header').empty()
+        $('<h3><span>' + uname + ' </span><small>退出</small></h3>')
+        .appendTo('header'),
+        $('header small').click(function(){
+            $.get('/user/signout' , null ,function(res){
+                if(res.code == 'success'){
+                    location.href = '/'
+                }
+            })
+        })
+
+$('header span').click(function(){
+    location.href = '/user.html'
+})
+}
